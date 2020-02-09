119d365391b90da82582cfdb8bedb7455caf314c
Daniel Chasman-Beck <Daniel@Dannys-MacBook-Pro.local>
2020-02-08 16:58:03 -0800

    add checker board set up

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/*----- constants -----*/
+
+
+/*----- app's state (variables) -----*/
+
+
+/*----- cached element references -----*/
+const playSqrs = document.querySelectorAll('.used');
+let lily = document.querySelector('div');
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+function initRender() {
+    // playSqrs.forEach(e => e.style.backgroundImage = 'images/lilly-pad-clipart-12.png')
+    // lily.style.backgroundImage = `url(images/lilly-pad-clipart-12.png)`;
+};
+
+function render() {
+    
+};
+
+initRender();

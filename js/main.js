754efd8875bddfb9a2feaf1ba8a84808969dece8
Daniel Chasman-Beck <Daniel@Dannys-MacBook-Pro.local>
2020-02-11 11:39:13 -0800

    reorganize and refacture functions

--- js/main.js
+++ js/main.js
@@ -12,12 +12,22 @@ class Piece {
     // move behavior
 
     move() {
+        if (isKing) {
+            // king move logic
+        } else {
+            // piece move logic
+        }
         // check if king
         // move forward diagonal
         // pass in variable of selected piece
         // pass in variable of desired space
     }
     jump() {
+        if (isKing) {
+            // king jump logic
+        } else {
+            // piece jump logic
+        }
         // check if king
         // jump forward over one piece
         // check for available jump forward
@@ -50,32 +60,11 @@ function handleMove(evt) {
     let eventClasses = Array.from(evt.target.classList);
     if (eventClasses.includes(`team${playerIds[gameState.turn]}-piece`) || eventClasses.includes(`team${playerIds[gameState.turn]}-king`) || (gameState.selectedPiece !== null && eventClasses.includes('empty'))) {
         if (gameState.selectedPiece === null) {
-            debugger;
-            gameState.selectedPiece = evt.target;
-            gameState.selectedPiece.classList.add('selected');
+            setSelectedPiece(evt);
         } else if (gameState.desiredSqr === undefined) {
-            gameState.desiredSqr = evt.target;
-            if (gameState.desiredSqr.className === 'empty') {
-                gameState.board[sqrIdx] = new Piece(gameState.turn);
-                gameState.selectedPiece.classList.remove('selected');
-                gameState.board[playSqrs.indexOf(gameState.selectedPiece)] = null;
-                gameState.selectedPiece = null;
-                gameState.desiredSqr = undefined;
-                gameState.turn *= -1;
-            } else if (gameState.desiredSqr === gameState.selectedPiece) {
-                gameState.selectedPiece.classList.remove('selected');
-                gameState.selectedPiece = null;
-                gameState.desiredSqr = undefined;
-                return;
-            } else {
-                gameState.desiredSqr = undefined;
-                return;
-            }
-            // check if desired is valid move
-            // create new piece in desired location
+            setDesiredPiece(evt, sqrIdx);
         }
     }
-
     // check piece clicked against current turn
     // if click on piece
     // check if move or jump
@@ -86,27 +75,69 @@ function handleMove(evt) {
     // call kingMove or kingJump method
 
     // if landed on end square call kingMe function
-    // console.log(evt.target);
-    // console.log(gameState.selectedPiece);
     render();
 }
 
-function kingMe() {
-    // remove piece object
-    // create instance of king object
-}
 function render() {
     // change value in gamestate.board array of clicked piece
     // remove jumped pieces
     // update positions of pieces moved
     // remove pieces that reach the end
     // if pieces reach end, add kings
-
+    
     //update "knocked off lillypad" count?
     //update win count per player?
     renderBoard();
 }
 
+
+
+function init() {
+    gameState.board = new Array(32).fill(null);
+    for (let i = 0; i < 12; i++) gameState.board[i] = new Piece(1);
+    for (let i = 20; i < 32; i++) gameState.board[i] = new Piece(-1);
+    
+    gameState.turn = 1;
+    gameState.win = null;
+    resetSelectors();
+    // reset board
+    // update win count per player?
+    
+    // reset play state
+    // initialize starting pieces
+    // reset taken pieces count
+    // remove kings
+    render();
+}
+
+function setSelectedPiece(evt) {
+    gameState.selectedPiece = evt.target;
+    gameState.selectedPiece.classList.add('selected');
+}
+function setDesiredPiece(evt, sqrIdx) {
+    gameState.desiredSqr = evt.target;
+    if (gameState.desiredSqr.className === 'empty') {
+        gameState.board[sqrIdx] = new Piece(gameState.turn);
+        gameState.selectedPiece.classList.remove('selected');
+        gameState.board[playSqrs.indexOf(gameState.selectedPiece)] = null;
+        resetSelectors();
+        gameState.turn *= -1;
+    } else if (gameState.desiredSqr === gameState.selectedPiece) {
+        gameState.selectedPiece.classList.remove('selected');
+        resetSelectors();
+        return;
+    } else {
+        gameState.desiredSqr = undefined;
+        return;
+    }
+}
+function resetSelectors() {
+    gameState.selectedPiece = null;
+    gameState.desiredSqr = undefined;
+}
+function kingMe() {
+    // change isKing property to true
+}
 function renderBoard() {
     gameState.board.forEach((piece, idx) => {
         let sqr = playSqrs[idx];
@@ -123,34 +154,12 @@ function renderBoard() {
         }
     });
 }
-
 function updateClasses(sqr, class1, class2, class3, class4, class5) {
     sqr.classList.remove(class1);
     sqr.classList.remove(class2);
     sqr.classList.remove(class3);
     sqr.classList.remove(class4);
     sqr.classList.add(class5);
-}
-
-function init() {
-    gameState.board = new Array(32).fill(null);
-    for (let i = 0; i < 12; i++) gameState.board[i] = new Piece(1);
-    for (let i = 20; i < 32; i++) gameState.board[i] = new Piece(-1);
-
-    gameState.turn = 1;
-    gameState.win = null;
-    gameState.selectedPiece = null;
-    gameState.desiredSqr = undefined;
-    // call initRender function
-    // reset board
-    // update win count per player?
-
-    // reset play state
-    // initialize starting pieces
-    // reset taken pieces count
-    // remove kings
-    render();
-    console.log(gameState.turn);
 }
 
 init();
